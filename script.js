359fc705cc31b813c28877d628891f8091412026
Karoline Correa <ckaroline226@gmail.com>
2022-06-22 21:41:52 -0300

    Iniciando o projeto

---
+++ script.js
@@ -0,0 +1,19 @@
+var peso = [70]
+var altura = [1,69]
+
+function pesoealtura(imc) {
+    const media = calcularimc(pesoealtura)
+
+    if (imc <= 18,4) return 'Abaixo do peso'
+    if (imc => 24,9) return 'Peso normal'
+    if (imc => 18,4) return 'Sobrepeso'
+    if (imc => 34,9) return 'Obesidade 1'
+    if (imc => 39,9) return 'Obesidade 2'
+    if (imc => 40) return 'Obesidade 3'
+}
+
+
+
+
+
+//altura * altura / peso
